7b47b7a55582b1c3aac33069e9e2e2964adaf6ba
Himura <glagol15@gmail.com>
2015-04-27 09:53:55 +0300

    ignore

--- replacer.py
+++ replacer.py
@@ -1,11 +1,13 @@
 import os
 
 rulesFile = "rules.txt"
-startPath = "D:\\Docs\\Docs\\Source\\14_2"
-printrules = False
+startPath = "D:\\Docs\\PivotGridDataSource"
+exclude = {"PivotGridDataSource.md"}
+printrules = True
 
 debug = True
 tempdir = "temp"
+
 
 #************** Reading rules **************
 
@@ -23,7 +25,7 @@ for line in lines:
             tgtRules.append(line)
         i += 1
 nRules = len(srcRules)
-if nRules != len(tgtRules): print("Not all lines has a pair. Watch out!"); exit()
+if nRules != len(tgtRules): print("Some lines have no pair. Watch out!"); exit()
 
 if printrules:
     for i in range(nRules):
@@ -34,33 +36,34 @@ if printrules:
 #************** Let's go replacing! **************
 
 hash = 0
-batFileName = "!!!Compare.bat"
+batFileName = "!!! Compare.bat"
 batFile = open(os.path.join(tempdir, batFileName), "w")
 for root, dirs, files in os.walk(startPath):
     for name in files:
-        path = os.path.join(root, name)
-        type = name.split(".")[-1]
-        if type in {'desc', 'md', 'htm'}:
-            with open(path, "r") as f: source = f.read()
-            temp = source
-            for i in range(nRules): # Check each rule
-                if temp.find(srcRules[i]) > 0:
-                    temp = temp.replace(srcRules[i], tgtRules[i])
+        if name not in exclude:
+            path = os.path.join(root, name)
+            type = name.split(".")[-1]
+            if type in {'desc', 'md', 'htm'}:
+                with open(path, "r") as f: source = f.read()
+                temp = source
+                for i in range(nRules): # Check each rule
+                    if temp.find(srcRules[i]) > 0:
+                        temp = temp.replace(srcRules[i], tgtRules[i])
 
-            if temp != source:
-                target = temp
-                hash += 1
-                if debug:
-                    tmpSrcPath = os.path.join(tempdir, name.replace(".", "_src"+str(hash)+"."))
-                    with open(tmpSrcPath, "w") as f: f.write(source)
+                if temp != source:
+                    target = temp
+                    hash += 1
+                    if debug:
+                        tmpSrcPath = os.path.join(tempdir, name.replace(".", "_src"+str(hash)+"."))
+                        with open(tmpSrcPath, "w") as f: f.write(source)
 
-                    tmpTgtPath = os.path.join(tempdir, name.replace(".", "_tgt"+str(hash)+"."))
-                    with open(tmpTgtPath, "w") as f: f.write(target)
+                        tmpTgtPath = os.path.join(tempdir, name.replace(".", "_tgt"+str(hash)+"."))
+                        with open(tmpTgtPath, "w") as f: f.write(target)
 
-                    batFile.write('start "" "C:\\Program Files (x86)\\WinMerge\\WinMergeU.exe" /s "' + tmpSrcPath + '" "' + tmpTgtPath + '"\n')
-
-                #else: with open(path, "w") as f: f.write(target)  # !!! BEWARE ERRORS !!!!
-
+                        batFile.write('start "" "C:\\Program Files (x86)\\WinMerge\\WinMergeU.exe" /s "' + tmpSrcPath + '" "' + tmpTgtPath + '"\n')
+                    else:
+                        pass
+                        #with open(path, "w") as f: f.write(target)  # !!! BEWARE ERRORS !!!!
                 print("[OK] " + name)
 print("[FINISH] " + str(hash) + " files processed")
 batFile.close()
