36358b14f4c8e8d7ceb1d9535ee9d78a0d63f7bb
Himura Kazuto <glagol15@gmail.com>
2020-07-29 14:56:36 +0300

    Refactor legacy code

--- Replace!.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python replacer.py
-pause
--- replacer.py
+++ replacer.py
@@ -1,104 +1,105 @@
 import os
+import re
 import sys
+import json
 
-rulesFile = "Rules\\rules_major.txt"
-startPath = "D:\\Docs\\Docs\\Source\\15_2"
-include = { 'aspx', 'desc', 'md', 'htm', 'js', 'html' }
-exclude = { }
+#***************** Config *****************
 
-debug = False
-debugDir = "Debug"
-winMergePath = "C:\\Program Files (x86)\\WinMerge"
+config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.json')
+config = {
+    "rules_file": "sample_rules.txt",
+    "target_path": "sample_content",
+    "include_extensions": [ 'aspx', 'desc', 'md', 'htm', 'js', 'html' ],
+    "exclude_file_names": [ ],
+    "test_mode": False,
+    "test_target_dir": "replaced_content",
+    "comment_sign": "="
+}
+
+if os.path.isfile(config_file):
+    config = json.load(open(config_file, 'r', encoding='utf-8'))
+    for key in ("rules_file", "target_path", "test_target_dir"):
+        config[key] = config[key].replace('\\', os.sep).replace('/', os.sep)
+else:
+    json.dump(config, open(config_file, 'w', encoding='utf-8'), indent=4)
+    print(f'{config_file} file not found and was created. Please adapt it to your needs.')
+    exit(2)
+
+if config['test_mode'] and not os.path.isdir(config['test_target_dir']):
+    os.makedirs(config['test_target_dir'])
 
 #************** Reading rules **************
 
-srcRules = []
-tgtRules = []
+source_rules = []
+target_rules = []
 
-with open(rulesFile, "r") as f: lines = f.readlines()
-i = 0
-for line in lines:
-    if line[0] not in {'=', '-'} and len(line) > 3:
-        line = line.split("\r")[0].split("\n")[0]
-        if i % 2 == 0:
-            srcRules.append(line)
-        else:
-            tgtRules.append(line)
-        i += 1
-nRules = len(srcRules)
-if nRules != len(tgtRules): print("[ERROR!!!] Some lines have no pair. Watch out!"); exit()
-
+with open(config['rules_file'], "r") as f: lines = f.readlines()
+lines = list(filter(lambda line: not (config['comment_sign'] and line.startswith(config['comment_sign'])) and re.match(r'\S', line), lines))
+for i, line in enumerate(lines):
+    line = line.strip('\r').strip('\n')
+    if i % 2 == 0:
+        source_rules.append(line)
+    else:
+        target_rules.append(line)
+rules_count = len(source_rules)
+if rules_count != len(target_rules):
+    print("[ERROR] Some rule lines have no pair.")
+    exit(1)
 print("Rules:")
-for i in range(nRules):
-    print(srcRules[i])
-    print(tgtRules[i])
-    print()
-
-
+for i in range(rules_count):
+    print(f'{source_rules[i]}\n{target_rules[i]}\n')
 
 #************** Counting files **************
 
 print("[INFO] Counting files...", end="", flush=True)
-totalFiles = 0
-for root, dirs, files in os.walk(startPath):
+total_files = 0
+for root, dirs, files in os.walk(config['target_path']):
     for name in files:
-        if name not in exclude:
+        if name not in config['exclude_file_names']:
             path = os.path.join(root, name)
             type = name.split(".")[-1]
-            if type in include: 
-                totalFiles += 1
-print("\r[OK] Total files: ", totalFiles)
-currentFile = 0
+            if type in config['include_extensions']:
+                total_files += 1
+print("\r[OK] Total files: ", total_files)
 
 #************** Let's go replacing! **************
 
-hash = 0
-batFileName = "!!! Compare.bat"
-batFile = open(os.path.join(debugDir, batFileName), "w")
-for root, dirs, files in os.walk(startPath):
+current_file = 0
+changed_files = 0
+
+for root, dirs, files in os.walk(config['target_path']):
     for name in files:
-        if name not in exclude:
+        if name not in config['exclude_file_names']:
             path = os.path.join(root, name)
             type = name.split(".")[-1]
-            if type in include:
+            if type in config['include_extensions']:
                 try:
                     with open(path, "r") as f: source = f.read()
-                    temp = source
-                    for i in range(nRules): # Check each rule
-                        if temp.find(srcRules[i]) > 0:
-                            temp = temp.replace(srcRules[i], tgtRules[i])
-    
-                    if temp != source:
-                        target = temp
-                        hash += 1
-                        if debug:
-                            srcPath = name.replace(".", "_src["+str(hash)+"].")
-                            tmpSrcPath = os.path.join(debugDir, srcPath)
-                            with open(tmpSrcPath, "w") as f: f.write(source)
-    
-                            tgtPath = name.replace(".", "_tgt["+str(hash)+"].")
-                            tmpTgtPath = os.path.join(debugDir, tgtPath)
-                            with open(tmpTgtPath, "w") as f: f.write(target)
-    
-                            batFile.write('start "" "' + 
-                                          os.path.join(winMergePath, "WinMergeU.exe") + 
-                                          '" /s "' + srcPath + '" "' + tgtPath + '"\n')
+                    buffer = source
+                    for i in range(rules_count):
+                        if buffer.find(source_rules[i]) > 0:
+                            buffer = buffer.replace(source_rules[i], target_rules[i])
+
+                    if buffer != source:
+                        if config['test_mode']:
+                            relative_path = path.replace(config['target_path'], '').strip(os.sep)
+                            tmpTgtPath = os.path.join(config['test_target_dir'], relative_path)
+                            with open(tmpTgtPath, "w") as f: f.write(buffer)
                         else:
                             pass
-                            with open(path, "w") as f: f.write(target)  # !!! BEWARE ERRORS !!!!
-                    currentFile += 1
-                    percentage = currentFile / totalFiles * 100
+                            with open(path, "w") as f: f.write(buffer)  # !!! BEWARE ERRORS !!!!
+                        changed_files += 1
+                    current_file += 1
+                    percentage = current_file / total_files * 100
 
-                    progressString = "\r[" + str(round(percentage)) + "%] " + \
+                    progress_string = "\r[" + str(round(percentage)) + "%] " + \
                                      name.encode(sys.stdout.encoding, errors='replace').decode(sys.stdout.encoding)
-                    print(progressString.ljust(80 - len(progressString)), end="", flush=True)
-                    
-                    
+                    print(progress_string.ljust(80 - len(progress_string)), end="", flush=True)
+
                 except Exception as e:
-                    print("\r[ERROR!!!] File: ", path)
+                    print("\r[ERROR!!!] File:", path)
                     print("-"*79)
                     print(e)
                     print("-"*79)
-finishString = "\r[FINISH] " + str(hash) + " files processed. Happy checking =)"
+finishString = f"\r[FINISH] {changed_files} files processed. Happy checking =)"
 print(finishString.ljust(80 - len(finishString)))
-batFile.close()
